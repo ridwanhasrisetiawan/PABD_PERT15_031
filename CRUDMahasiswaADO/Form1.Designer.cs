05b272f2cbf6cba599f7f0a99e56d6ff9ace1193
ridwanhasrisetiawan <ridwanhasrisetiawan@gmail.com>
2026-06-20 19:44:40 +0700

    Designer untuk form 1

---
+++ CRUDMahasiswaADO/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace CRUDMahasiswaADO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCetak = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.cmbProdi = new System.Windows.Forms.ComboBox();
+            this.lblProdi = new System.Windows.Forms.Label();
+            this.lblTahunMasuk = new System.Windows.Forms.Label();
+            this.dtpTanggalMasuk = new System.Windows.Forms.DateTimePicker();
+            this.btnLoad = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(320, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(230, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "REKAP DATA MAHASISWA";
+            // 
+            // btnCetak
+            // 
+            this.btnCetak.Location = new System.Drawing.Point(713, 415);
+            this.btnCetak.Name = "btnCetak";
+            this.btnCetak.Size = new System.Drawing.Size(75, 23);
+            this.btnCetak.TabIndex = 1;
+            this.btnCetak.Text = "cetak";
+            this.btnCetak.UseVisualStyleBackColor = true;
+            this.btnCetak.Click += new System.EventHandler(this.btnCetak_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 68);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(776, 339);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // cmbProdi
+            // 
+            this.cmbProdi.FormattingEnabled = true;
+            this.cmbProdi.Items.AddRange(new object[] {
+            "TI",
+            "TE",
+            "TS",
+            "TM"});
+            this.cmbProdi.Location = new System.Drawing.Point(190, 37);
+            this.cmbProdi.Name = "cmbProdi";
+            this.cmbProdi.Size = new System.Drawing.Size(121, 24);
+            this.cmbProdi.TabIndex = 3;
+            this.cmbProdi.SelectedIndexChanged += new System.EventHandler(this.cmbProdi_SelectedIndexChanged);
+            // 
+            // lblProdi
+            // 
+            this.lblProdi.AutoSize = true;
+            this.lblProdi.Location = new System.Drawing.Point(122, 37);
+            this.lblProdi.Name = "lblProdi";
+            this.lblProdi.Size = new System.Drawing.Size(39, 16);
+            this.lblProdi.TabIndex = 4;
+            this.lblProdi.Text = "Prodi";
+            // 
+            // lblTahunMasuk
+            // 
+            this.lblTahunMasuk.AutoSize = true;
+            this.lblTahunMasuk.Location = new System.Drawing.Point(350, 37);
+            this.lblTahunMasuk.Name = "lblTahunMasuk";
+            this.lblTahunMasuk.Size = new System.Drawing.Size(88, 16);
+            this.lblTahunMasuk.TabIndex = 5;
+            this.lblTahunMasuk.Text = "Tahun Masuk";
+            // 
+            // dtpTanggalMasuk
+            // 
+            this.dtpTanggalMasuk.Location = new System.Drawing.Point(444, 39);
+            this.dtpTanggalMasuk.Name = "dtpTanggalMasuk";
+            this.dtpTanggalMasuk.Size = new System.Drawing.Size(200, 22);
+            this.dtpTanggalMasuk.TabIndex = 6;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(672, 39);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 7;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.dtpTanggalMasuk);
+            this.Controls.Add(this.lblTahunMasuk);
+            this.Controls.Add(this.lblProdi);
+            this.Controls.Add(this.cmbProdi);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnCetak);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCetak;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox cmbProdi;
+        private System.Windows.Forms.Label lblProdi;
+        private System.Windows.Forms.Label lblTahunMasuk;
+        private System.Windows.Forms.DateTimePicker dtpTanggalMasuk;
+        private System.Windows.Forms.Button btnLoad;
+    }
+}
