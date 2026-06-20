b260298229b44b5263da15519f0cbe395401c20b
ridwanhasrisetiawan <ridwanhasrisetiawan@gmail.com>
2026-06-20 19:27:38 +0700

    menambahkan code untuk form 1

---
+++ CRUDMahasiswaADO/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace CRUDMahasiswaADO
+{
+    public partial class Form1 : Form
+    {
+        DAL dbLogic = new DAL();
+
+        static string connectionString =
+            "Data Source=LAPTOP-4VAVDOFH\\WAWANLOMBOK;Initial Catalog=DBAkademikADO;User ID=sa;Password=123";
+
+        // FIX 1: Hapus field SqlConnection conn yang di-share.
+        //        Semua koneksi dibuka dengan 'using' di dalam masing-masing method.
+        SqlDataAdapter da;
+        DataTable dtMahasiswa;
+        DataTable dtProdi;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dtpTanggalMasuk.Format = DateTimePickerFormat.Custom;
+            dtpTanggalMasuk.CustomFormat = "yyyy";
+            dtpTanggalMasuk.ShowUpDown = true;
+            dtpTanggalMasuk.MinDate = new DateTime(2000, 1, 1);
+            dtpTanggalMasuk.MaxDate = DateTime.Now;
+
+            cmbProdi.DropDownStyle = ComboBoxStyle.DropDownList;
+            btnCetak.Enabled = false;
+
+            try
+            {
+                // FIX 2: Gunakan 'using' untuk koneksi saat load data prodi
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand("select namaprodi from programstudi", conn);
+                    cmd.CommandType = CommandType.Text;
+                    dtProdi = new DataTable();
+                    da = new SqlDataAdapter(cmd);
+                    da.Fill(dtProdi);
+                }
+
+                cmbProdi.DataSource = dtProdi;
+                cmbProdi.DisplayMember = "namaprodi";
+                cmbProdi.ValueMember = "namaprodi";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Gagal load data prodi: " + ex.Message);
+            }
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            // FIX 3: Validasi combobox sebelum query
+            if (cmbProdi.SelectedValue == null)
+            {
+                MessageBox.Show("Pilih prodi terlebih dahulu!");
+                return;
+            }
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand("sp_Report", conn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add("@inProdi", SqlDbType.VarChar, 50).Value = cmbProdi.SelectedValue;
+                    cmd.Parameters.Add("@inTglMsuk", SqlDbType.VarChar, 4).Value = dtpTanggalMasuk.Value.Year.ToString();
+
+                    da = new SqlDataAdapter(cmd);
+                    dtMahasiswa = new DataTable();
+                    da.Fill(dtMahasiswa);
+                }
+
+                dataGridView1.DataSource = dtMahasiswa;
+
+                if (dtMahasiswa.Rows.Count > 0)
+                {
+                    btnCetak.Enabled = true;
+                }
+                else
+                {
+                    btnCetak.Enabled = false;
+                    MessageBox.Show("Data tidak ditemukan.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Gagal load data: " + ex.Message);
+            }
+        }
+
+        private void btnCetak_Click(object sender, EventArgs e)
+        {
+            string prodi = cmbProdi.SelectedValue.ToString();
+            DateTime tglMasuk = dtpTanggalMasuk.Value;
+
+            Form2 frm = new Form2(prodi, tglMasuk);
+            frm.ShowDialog();
+        }
+
+        private void cmbProdi_SelectedIndexChanged(object sender, EventArgs e) { }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e) { }
+    }
+}
