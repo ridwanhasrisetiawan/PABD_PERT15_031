dc271830c69a17669055ee94f302be5c74852bfe
ridwanhasrisetiawan <ridwanhasrisetiawan@gmail.com>
2026-06-20 19:36:51 +0700

    menambahkan code untuk dashboard

---
+++ CRUDMahasiswaADO/DASHBOARD.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace CRUDMahasiswaADO
+{
+    public partial class DASHBOARD : Form
+    {
+        DAL dbLogic = new DAL();
+        bool isInitializing = true;
+        DataTable dt;
+        int button = 0;
+
+        public DASHBOARD()
+        {
+            InitializeComponent();
+
+            dtpTanggalMasuk.MinDate = new DateTime(2000, 1, 1);
+            dtpTanggalMasuk.Format = DateTimePickerFormat.Custom;
+            dtpTanggalMasuk.CustomFormat = "yyyy";
+            dtpTanggalMasuk.ShowUpDown = true;
+            dtpTanggalMasuk.MaxDate = DateTime.Now;
+
+            cmbTipe.DropDownStyle = ComboBoxStyle.DropDownList;
+
+            var items = new List<KeyValuePair<string, SeriesChartType>>
+            {
+                new KeyValuePair<string, SeriesChartType>("Kolom", SeriesChartType.Column),
+                new KeyValuePair<string, SeriesChartType>("Pie", SeriesChartType.Pie)
+            };
+
+            isInitializing = true;
+
+            cmbTipe.DataSource = items;
+            cmbTipe.DisplayMember = "Key";
+            cmbTipe.ValueMember = "Value";
+            cmbTipe.SelectedIndex = 0;
+
+            isInitializing = false;
+
+            loadDataChart();
+        }
+
+        private void loadDataChart()
+        {
+            chartProdi.Series.Clear();
+            chartProdi.Titles.Clear();
+            chartProdi.Legends.Clear();
+            chartProdi.ChartAreas.Clear();
+
+            ChartArea ca = new ChartArea("MainArea");
+            ca.AxisX.Title = "Program Studi";
+            ca.AxisY.Title = "Jumlah Mahasiswa";
+            ca.AxisY.LabelStyle.Angle = -45;
+            ca.BackColor = Color.Transparent;
+            chartProdi.ChartAreas.Add(ca);
+
+            try
+            {
+                if (button == 1)
+                {
+                    dt = dbLogic.getDataChartByTahun(dtpTanggalMasuk.Value);
+                }
+                else
+                {
+                    dt = dbLogic.getAllDataChart();
+                }
+
+                SeriesChartType tipe = (SeriesChartType)cmbTipe.SelectedValue;
+
+                if (tipe == SeriesChartType.Column)
+                {
+                    Series s = new Series("Mahasiswa");
+                    s.ChartType = SeriesChartType.Column;
+                    s.IsValueShownAsLabel = true;
+
+                    foreach (DataRow row in dt.Rows)
+                    {
+                        string prodi = row["NamaProdi"].ToString();
+                        int jumlah = Convert.ToInt32(row["JmlMhs"]);
+                        s.Points.AddXY(prodi, jumlah);
+                    }
+
+                    chartProdi.Series.Add(s);
+                }
+                else
+                {
+                    Series s = new Series("Jumlah Mahasiswa");
+                    s.ChartType = tipe;
+                    s.IsValueShownAsLabel = true;
+                    s.Label = "#VAL";
+                    s.LegendText = "#VALX";
+
+                    foreach (DataRow row in dt.Rows)
+                    {
+                        string prodi = row["NamaProdi"].ToString();
+                        int jumlah = Convert.ToInt32(row["JmlMhs"]);
+                        s.Points.AddXY(prodi, jumlah);
+                    }
+
+                    chartProdi.Series.Add(s);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Gagal load data: " + ex.Message);
+            }
+
+            Title title = new Title(
+                "Jumlah Mahasiswa per Program Studi",
+                Docking.Top,
+                new Font("Arial", 14, FontStyle.Bold),
+                Color.DarkBlue
+            );
+
+  
+        }
+
+        private void DASHBOARD_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmbTipe_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (isInitializing)
+                return;
+
+            if (button == 1)
+            {
+
+            }
+            else
+            {
+                loadDataChart();
+            }
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            button = 1;
+            loadDataChart();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            button = 0;
+            loadDataChart();
+        }
+
+        private void btnDataMahasiswa_Click(object sender, EventArgs e)
+        {
+            FormMahasiswa frm1 = new FormMahasiswa();
+            frm1.Show();
+            this.Hide();
+        }
+    }
+}
